d5ea23e656bd53eabf9df895b7e10884c254d5e2
unknown <ahmed_elsayed@codebase-tech.com>
2022-09-17 16:02:48 +0200

    WIP

--- routes/frontend/frontend.router.php
+++ routes/frontend/frontend.router.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return 'helsdsdlo-world';
+    return 'hello-world';
 });
 
 Route::post('pull-from-github','PullFromGithubController')->withoutMiddleware([VerifyCsrfToken::class]);
