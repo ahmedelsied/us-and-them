a3065680b3d275f31f3a643848f3703fcada0c18
unknown <ahmed_elsayed@codebase-tech.com>
2022-09-17 15:59:51 +0200

    WIP

--- routes/frontend/frontend.router.php
+++ routes/frontend/frontend.router.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return 'helsddlo-world';
+    return 'hello-world';
 });
 
 Route::post('pull-from-github','PullFromGithubController');
