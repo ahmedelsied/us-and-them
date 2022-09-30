e4dcfa3e9771d048a4a1e6e936e9791d95ce0ecb
unknown <ahmed_elsayed@codebase-tech.com>
2022-09-30 21:43:18 +0200

    WIP

--- resources/views/frontend/static-pages/terms-and-conditions.blade.php
+++ resources/views/frontend/static-pages/terms-and-conditions.blade.php
@@ -22,7 +22,7 @@
 
     <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Us and them if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-    <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+    <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
     <h3><strong>Cookies</strong></h3>
 
@@ -32,7 +32,7 @@
 
     <h3><strong>License</strong></h3>
 
-    <p>Unless otherwise stated, Us and them and/or its licensors own the intellectual property rights for all material on Us and them. All intellectual property rights are reserved. You may access this from Us and them for your own personal use subjected to restrictions set in these terms and conditions.</p>
+    <p>Unless otherwise stated, Us and them and its licensors own the intellectual property rights for all material on Us and them. All intellectual property rights are reserved. You may access this from Us and them for your own personal use subjected to restrictions set in these terms and conditions.</p>
 
     <p>You must not:</p>
     <ul>
@@ -44,7 +44,7 @@
 
     <p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a href="https://www.termsandconditionsgenerator.com/">Free Terms and Conditions Generator</a>.</p>
 
-    <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Us and them does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Us and them,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Us and them shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
+    <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Us and them does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Us and them,its agents and affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Us and them shall not be liable for the Comments or for any liability, damages or expenses caused and suffered as a result of any use of and posting of and appearance of the Comments on this website.</p>
 
     <p>Us and them reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
 
@@ -71,12 +71,12 @@
         <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
     </ul>
 
-    <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.</p>
+    <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and services; and (c) fits within the context of the linking party’s site.</p>
 
     <p>We may consider and approve other link requests from the following types of organizations:</p>
 
     <ul>
-        <li>commonly-known consumer and/or business information sources;</li>
+        <li>commonly-known consumer and business information sources;</li>
         <li>dot.com community sites;</li>
         <li>associations or other groups representing charities;</li>
         <li>online directory distributors;</li>
