7f1d1af29b1d1e47383a4d69e969a488f22a65f1
unknown <ahmed_elsayed@codebase-tech.com>
2022-09-30 22:13:55 +0200

    WIP

--- app/Http/Controllers/Frontend/PullFromGithubController.php
+++ app/Http/Controllers/Frontend/PullFromGithubController.php
@@ -8,6 +8,7 @@ class PullFromGithubController extends Controller
 {
     public function __invoke()
     {
+        shell_exec('git reset --hard');
         shell_exec('git pull origin main');
         shell_exec('php '.base_path().'/artisan migrate');
         shell_exec('php '.base_path().'/artisan optimize:clear');
