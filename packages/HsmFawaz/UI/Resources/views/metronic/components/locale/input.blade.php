7415fe5e6860ec86dfc64801677bc5e18471798a
unknown <ahmed_elsayed@codebase-tech.com>
2022-10-13 23:09:50 +0200

    WIP

--- packages/HsmFawaz/UI/Resources/views/metronic/components/locale/input.blade.php
+++ packages/HsmFawaz/UI/Resources/views/metronic/components/locale/input.blade.php
@@ -1,12 +1,12 @@
 @props(['name','label','value'=>null])
 <div class="row">
     <div class="col-12 col-md-6">
-        <x-ui::form.input :name="$name.'[ar]'"
+        <x-ui::form.input :name="$name.'[ar]'"  dir="rtl"
                       {{ $attributes }} :label="__ (':label Ar',['label'=>$label])"
                       :value="data_get($value,'ar') ?? locale_field($name)" {{$attributes}} />
     </div>
     <div class="col-12 col-md-6">
-        <x-ui::form.input :name="$name.'[en]'"
+        <x-ui::form.input :name="$name.'[en]'" dir="ltr"
                       {{ $attributes }} :label="__ (':label En',['label'=>$label])"
                       :value="data_get($value,'en') ?? locale_field($name,'en')" {{$attributes}}/>
     </div>
